4f011c6c445b6f56d637aa65f0a33f111f594b2d
FathullaLatipov <fathullaxonlatipov@gmail.com>
2024-06-05 15:27:35 +0500

    models for games

--- games/models.py
+++ games/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class GenreModel(models.Model):
+    title = models.CharField(max_length=130, verbose_name='Жанр', help_text='Тут надо указать жанр')
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
+class GameModeModels(models.Model):
+    title = models.CharField(max_length=130, verbose_name='Режим игры', help_text='Тут надо указать режим игры')
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Режим игры'
+        verbose_name_plural = 'Режим игры'
+
+
+class PlatformModels(models.Model):
+    title = models.CharField(max_length=130, verbose_name='Платформа', help_text='Тут надо указать тип платформ')
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Платформа'
+        verbose_name_plural = 'Платформы'
